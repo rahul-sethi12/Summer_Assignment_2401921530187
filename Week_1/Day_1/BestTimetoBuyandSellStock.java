514141cb5a7bc43137a6290690f9a6c4b4179598
Rahul_Sethi <rahulsethi1712@gmail.com>
2026-06-03 14:56:52 +0530

    Create BestTimetoBuyandSellStock.java

---
+++ Week_1/Day_1/BestTimetoBuyandSellStock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for(int i =0;i<n;i++){
+            if(prices[i]<min){
+                min = prices[i];
+            }
+            max = Math.max(max,prices[i]-min);
+        }
+        
+        return max;
+    }
+}
